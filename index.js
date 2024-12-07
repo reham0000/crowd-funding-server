14cf5b2e5dca66f88271dcf81080f1f414ab5225
reham0000 <amsarkerreham@gmail.com>
2024-12-07 22:20:56 +0600

    take all campaigns data from db

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
-
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.w4irz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,28 +17,60 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const fundCollection = client.db('fundDB').collection('fund');
+
+
+    app.get('/fund', async(req, res) => {
+        const cursor = fundCollection.find().limit(6); 
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+
+    app.get('/funds', async(req, res) => {
+        const cursor = fundCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/fund/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+        const result = await fundCollection.findOne(query);
+        res.send(result);
+    })
+
+    app.post('/fund', async(req, res) => {
+        const newFund = req.body;
+        console.log(newFund);
+        const result = await fundCollection.insertOne(newFund);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-    res.send('crowd funding server is running');
-})
+app.get("/", (req, res) => {
+  res.send("crowd funding server is running");
+});
 
 app.listen(port, () => {
-    console.log(`crowd funding server is running on port: ${port}`);
-})
+  console.log(`crowd funding server is running on port: ${port}`);
+});
