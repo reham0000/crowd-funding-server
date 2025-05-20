e188f97370547e898fd6e09477b1e23c555aa266
reham0000 <amsarkerreham@gmail.com>
2025-05-20 21:40:00 +0600

    added payment gateway

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const express = require("express");
+const SSLCommerzPayment = require("sslcommerz-lts");
+const cors = require("cors");
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -20,10 +23,14 @@ const client = new MongoClient(uri, {
   },
 });
 
+const store_id = process.env.STORE_ID;
+const store_passwd = process.env.STORE_PASS;
+const is_live = false; //true for live, false for sandbox
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const fundCollection = client.db("fundDB").collection("fund");
 
@@ -31,9 +38,9 @@ async function run() {
 
     const donationCollection = client.db("fundDB").collection("donation");
 
-    const newCollection = client.db("fundDB").collection("add")
+    const newCollection = client.db("fundDB").collection("add");
 
-    // db.orders.find().sort( { amount: -1 } )
+    const moneyCollection = client.db("fundDB").collection("money");
 
     app.get("/fund", async (req, res) => {
       const cursor = fundCollection.find().limit(6);
@@ -67,42 +74,40 @@ async function run() {
       res.send(result);
     });
 
-
-
-    app.get("/myfunds/:id", async(req, res) => {
+    app.get("/myfunds/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await fundCollection.findOne(query);
       res.send(result);
-    })
+    });
 
-    app.put("/myfunds/:id", async(req, res) => {
+    app.put("/myfunds/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id:new ObjectId(id)}
-      const options = {upsert: true};
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
       const updatedCampaign = req.body;
       const campaign = {
         $set: {
-          deadline:updatedCampaign.deadline,
-          description:updatedCampaign.description, 
-          email:updatedCampaign.email, 
-          minDonation:updatedCampaign.minDonation, 
-          name:updatedCampaign.name, 
-          thumbnail: updatedCampaign.thumbnail, 
-          title:updatedCampaign.title, 
-          type: updatedCampaign.type
-        }
-      }
+          deadline: updatedCampaign.deadline,
+          description: updatedCampaign.description,
+          email: updatedCampaign.email,
+          minDonation: updatedCampaign.minDonation,
+          name: updatedCampaign.name,
+          thumbnail: updatedCampaign.thumbnail,
+          title: updatedCampaign.title,
+          type: updatedCampaign.type,
+        },
+      };
       const result = await fundCollection.updateOne(filter, campaign, options);
-      res.send(result); 
-    })
+      res.send(result);
+    });
 
-    app.delete("/myfunds/:id", async(req, res) => {
+    app.delete("/myfunds/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await fundCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     // users related apis
 
@@ -134,10 +139,114 @@ async function run() {
       res.send(result);
     });
 
-    
+    //payment intent
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = parseInt(price * 100);
+      console.log(amount, "amount inside  the intent");
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card "],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    //sslCommerz
+
+    const tran_id = new ObjectId().toString();
+
+    app.post("/order", async (req, res) => {
+      // const help = await fundCollection.findOne({
+      //   _id:new ObjectId(req.body.uid),
+      // });
+      const order = req.body;
+
+      const data = {
+        total_amount: order?.minDonation,
+        currency: "BDT",
+        tran_id: tran_id, // use unique tran_id for each api call
+        success_url: `http://localhost:5000/payment/success/${tran_id}`,
+        fail_url: `http://localhost:5000/payment/fail/${tran_id}`,
+        cancel_url: "http://localhost:3030/cancel",
+        ipn_url: "http://localhost:3030/ipn",
+        shipping_method: "Courier",
+        product_name: order?.name,
+        product_category: "Electronic",
+        product_profile: "general",
+        cus_name: order?.name,
+        cus_email: order?.email,
+        cus_add1: "Dhaka",
+        cus_add2: "Dhaka",
+        cus_city: "Dhaka",
+        cus_state: "Dhaka",
+        cus_postcode: "1000",
+        cus_country: "Bangladesh",
+        cus_phone: "01711111111",
+        cus_fax: "01711111111",
+        ship_name: "Customer Name",
+        ship_add1: "Dhaka",
+        ship_add2: "Dhaka",
+        ship_city: "Dhaka",
+        ship_state: "Dhaka",
+        ship_postcode: 1000,
+        ship_country: "Bangladesh",
+      };
+
+      console.log(data);
+
+      const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live);
+      sslcz.init(data).then((apiResponse) => {
+        // Redirect the user to payment gateway
+        let GatewayPageURL = apiResponse.GatewayPageURL;
+        res.send({ url: GatewayPageURL });
+
+        const finalDonation = {
+          order,
+          paidStatus: false,
+          transactionId: tran_id,
+        };
+        const result = moneyCollection.insertOne(finalDonation);
+
+        console.log("Redirecting to: ", GatewayPageURL);
+      });
+
+      app.post("/payment/success/:tranId", async (req, res) => {
+        console.log(req.params.tranId);
+        const result = await moneyCollection.updateOne(
+          { transactionId: req.params.tranId },
+          {
+            $set: {
+              paidStatus: true,
+            },
+          }
+        );
+
+        if (result.modifiedCount > 0) {
+          res.redirect(
+            `http://localhost:5173/payment/success/${req.params.tranId}`
+          );
+        }
+      });
+
+      app.post("/payment/fail/:tranId", async (req, res) => {
+        const result = await moneyCollection.deleteOne({
+          transactionId: req.params.tranId,
+        });
+
+        if (result.deletedCount) {
+          res.redirect(
+            `http://localhost:5173/payment/fail/${req.params.tranId}`
+          );
+        }
+      });
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
